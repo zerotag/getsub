84f5f83e2316089dc37e84da58825d4e7ea66f7a
zeroTAG <zerotag@users.noreply.github.com>
2019-04-03 19:52:10 -0300

    Merge Update (#1)
    * Update getsub.py
    
    * Update getsub.py
    
    * Update getsub.py
    
    * Create .gitignore
    
    * Update .gitignore
    
    * Update getsub.py

--- getsub.py
+++ getsub.py
@@ -5,30 +5,34 @@ import hashlib
 import requests
 
 user_agent = {'User-Agent': "SubDB/1.0 (getsub/0.1; http://github.com/trecuu/getsub)"}
+language = 'pt,en'#selected languages
+videoFile = str(sys.argv[1]) #file location ex.: C:\Downloads\Video.mkv
+
 
 def get_hash(name):
-        readsize = 64 * 1024
-        with open(name, 'rb') as f:
-            size = os.path.getsize(name)
-            data = f.read(readsize)
-            f.seek(-readsize, os.SEEK_END)
-            data += f.read(readsize)
-        return hashlib.md5(data).hexdigest()
+	readsize = 64 * 1024
+	with open(name, 'rb') as f:
+		size = os.path.getsize(name)
+		data = f.read(readsize)
+		f.seek(-readsize, os.SEEK_END)
+		data += f.read(readsize)
+	return hashlib.md5(data).hexdigest()
 
-def get_sub(fileHash):
-	url = 'http://api.thesubdb.com/?action=download&hash='+fileHash+'&language=pt,en'
-	r = requests.get(url, headers=user_agent)
-	if str(r.status_code) == '404':
+
+def get_sub(fileHash, language):
+	url = 'http://api.thesubdb.com/?action=download&hash='+fileHash+'&language='+language 
+	url = requests.get(url, headers=user_agent)
+	if str(url.status_code) == '404':
 		print ('Not Found')
-	elif str(r.status_code) == '400':
+	elif str(url.status_code) == '400':
 		print ('Bad Request')
-	elif str(r.status_code) == '200':
-		open(str(sys.argv[1])+'.srt', 'wb').write(r.content)
+	elif str(url.status_code) == '200':
+		open(videoFile[:-4]+'.srt', 'wb').write(url.content)
 		print ('SUB DOWNLOADED!')
 
 
 
-videoHash = get_hash(str(sys.argv[1]))
-get_sub(videoHash)
+videoHash = get_hash(videoFile)
+get_sub(videoHash,language)
 
 time.sleep(1)
